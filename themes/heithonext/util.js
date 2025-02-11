b334226fd134f7c1dd4e7644739c5f76877efebe
zjt <lilzjt@163.com>
2025-02-11 14:35:54 +0800

    fix: 修复依赖

--- themes/heithonext/util.js
+++ themes/heithonext/util.js
@@ -9,6 +9,20 @@ export function checkContainHttp(str) {
   }
 }
 
+// 转换外链
+export function sliceUrlFromHttp(str) {
+  // 检查字符串是否包含http
+  if (str.includes('http:') || str.includes('https:')) {
+    // 如果包含，找到http的位置
+    const index = str.indexOf('http')
+    // 返回http之后的部分
+    return str.slice(index, str.length)
+  } else {
+    // 如果不包含，返回原字符串
+    return str
+  }
+}
+
 /**
  * 保存深色模式
  * @param newTheme
